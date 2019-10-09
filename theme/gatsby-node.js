5aede6bd46e2596fed271dab5f8942b572c7f4b6
lwz7512 <lwz7512@gmail.com>
2019-10-09 11:16:51 -0300

    make gatsby-node.js more neat...

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -39,38 +39,34 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 }
 
 // 2. then create pages by template & .md content
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   
   const { createPage } = actions
   const categoryTplt = require.resolve(`./src/templates/category.js`)
   const tutorialTplt = require.resolve(`./src/templates/tutorial.js`)
   const quizTplt     = require.resolve(`./src/templates/quiz.js`)
-  
-  return graphql(
-    `
-      {
-        pages: allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                template
-              }
-            }
+  const markdowns = `{
+    pages: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 1000
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            template
           }
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
     }
+  }`
 
+  try {
+    console.log('>>> start query markdowns...')
+    const result = await graphql(markdowns)
+    console.log('>>> start generate pages...')
     // Create site pages by .md file
     const pages = result.data.pages.edges;
 
@@ -136,7 +132,10 @@ exports.createPages = ({ graphql, actions }) => {
 
     // end of pages loop
     });
-  // end of graphql resolve callback
-  });
+
+  } catch (error) {
+    console.error(error);
+  }
+ 
 // end of createPages
 }
